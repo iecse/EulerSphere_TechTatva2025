f745c8ee9c428fa40b5662d76c70886c78e036ca
IshitaPadhi <ishitapadhi2005@gmail.com>
2025-08-30 01:36:06 +0530

    mew qs

--- Round1/IshitaPadhi/10.RightTriangleSquares/RightTriangleSquares.java
+++ Round1/IshitaPadhi/10.RightTriangleSquares/RightTriangleSquares.java
@@ -3,20 +3,36 @@ public class RightTriangleSquares {
         int N = 100; // given input
         int count = 0;
 
-        for (int a = 1; a < N; a++) {
-            for (int b = a + 1; b < N; b++) {
-                int c2 = a * a + b * b;
-                int c = (int) Math.sqrt(c2);
+        for (int m = 2; m * m < N; m++) {
+            for (int n = 1; n < m; n++) {
+                if (((m - n) % 2 == 1) && gcd(m, n) == 1) {
+                    int a = m * m - n * n;
+                    int b = 2 * m * n;
+                    int c = m * m + n * n;
 
-                if (c > N) continue;
-                if (c * c != c2) continue; // must be perfect square hypotenuse
+                    if (c > N) continue;
 
-                if ((c - b) > 0 && c % (c - b) == 0) {
-                    count++;
+                    // scale primitive triples
+                    int k = 1;
+                    while (k * c <= N) {
+                        int A = k * a;
+                        int B = k * b;
+                        int C = k * c;
+
+                        int big = Math.max(A, B);
+                        if ((C - big) > 0 && C % (C - big) == 0) {
+                            count++;
+                        }
+                        k++;
+                    }
                 }
             }
         }
 
         System.out.println("Number of valid triples = " + count);
+    }
+
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
