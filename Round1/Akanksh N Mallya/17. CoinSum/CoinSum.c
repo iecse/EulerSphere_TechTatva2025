c2bf6b5ef88ee526828247d1024e51e84d3e32d2
Akanksh N Mallya <akankshmallya@gmail.com>
2025-09-25 10:27:27 +0530

    Questions updated

--- Round1/Akanksh N Mallya/17. CoinSum/CoinSum.c
+++ Round1/Akanksh N Mallya/17. CoinSum/CoinSum.c
@@ -1,22 +1,29 @@
-#include <stdio.h>
+// Returns the count of ways we can
+// sum coins[0...n-1] coins to get sum "sum"
+int countRecur(int coins[], int n, int sum) {
+    // If sum is 0 then there is 1 solution
+    // (do not include any coin)
+    if (sum == 0)
+        return 1;
 
-int main(void) {
-    const int target = 2500; // 2.5 kg in grams
-    const int coins[] = {1, 5, 25, 50, 100, 1000, 2500};
-    const int n = sizeof(coins) / sizeof(coins[0]);
+    // 0 ways in the following two cases
+    if (sum < 0 || n == 0)
+        return 0;
 
-    long long dp[target + 1];
-    for (int i = 0; i <= target; ++i) dp[i] = 0;
-    dp[0] = 1; // one way to make 0
+    // count is sum of solutions (i)
+    // including coins[n-1] (ii) excluding coins[n-1]
+    return countRecur(coins, n, sum - coins[n - 1]) +
+           countRecur(coins, n - 1, sum);
+}
 
-    // Count combinations (order doesn't matter)
-    for (int i = 0; i < n; ++i) {
-        int c = coins[i];
-        for (int a = c; a <= target; ++a) {
-            dp[a] += dp[a - c];
-        }
-    }
+int count(int coins[], int n, int sum) {
+    return countRecur(coins, n, sum);
+}
 
-    printf("%lld\n", dp[target]); // prints 3444269
+int main() {
+    int coins[] = {5, 50, 100,1000,2500};
+    int n = sizeof(coins) / sizeof(coins[0]);
+    int sum = 2500;
+    printf("%d\n", count(coins, n, sum));
     return 0;
 }
--- Round1/Akanksh N Mallya/17. CoinSum/CoinSum.md
+++ Round1/Akanksh N Mallya/17. CoinSum/CoinSum.md
@@ -1,9 +1,24 @@
-### Question (Difficulty: Medium)
+---
+title: "The Master Gear Problem"
+difficulty: "Medium"
+tags: ["math", "combinatorics", "coin change", "dynamic programming"]
+id: 9
+---
 
-In the country of Great Cryptoss, the currency is called the **gear (g)**. Coins in circulation are:
+# The Master Gear Problem (Difficulty: Medium)
 
-**1g, 5g, 25g, 50g, 100g, 1kg, and 2.5kg**
+In the country of **Great Cryptoss**, the currency is called the **gear (g)**.  
+Coins in circulation are:
 
-It is possible to make **2.5kg** (which they call a "master gear") in several different ways using these coins.
+**5g, 50g, 100g, 1kg, and 2.5kg**
+
+It is possible to make **2.5kg** (which they call a *"master gear"*) in several different ways using these coins.
+
+---
 
 How many different ways can **2.5kg** be made using any number of the above coins?
+
+---
+## Solution
+969
+
